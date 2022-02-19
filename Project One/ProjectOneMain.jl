766c7398b5c885f6ded061ccaaf591befd4bf5c9
Brandon Kaplan <59454456+BrandonKaplan@users.noreply.github.com>
2022-02-19 11:38:15 -0800

    Added area for Excess Mortality

--- Project One/ProjectOneMain.jl
+++ Project One/ProjectOneMain.jl
@@ -46,4 +46,29 @@ function vaccinations()
     return vaccination_dict
 end 
 
+#=
+Processes the file excess_mortality.csv file with the format:
+location, date, p_scores_all_ages, p_scores_15_64, p_scores_65_74, p_scores_75_84, p_scores_85plus, 
+deaths_2020_all_ages, average_deaths_2015_2019_all_ages,deaths_2015_all_ages, deaths_2016_all_ages,
+deaths_2017_all_ages, deaths_2018_all_ages, deaths_2019_all_ages, deaths_2010_all_ages, 
+deaths_2011_all_ages, deaths_2012_all_ages, deaths_2013_all_ages, deaths_2014_all_ages, deaths_2021_all_ages,
+time,time_unit,p_scores_0_14, projected_deaths_2020_2022_all_ages, excess_proj_all_ages, cum_excess_proj_all_ages,
+cum_proj_deaths_all_ages, cum_p_proj_all_ages, p_proj_all_ages, p_proj_0_14,p_proj_15_64,
+p_proj_65_74, p_proj_75_84, p_proj_85p, cum_excess_per_million_proj_all_ages, excess_per_million_proj_all_ages,
+deaths_2022_all_ages,deaths_2020_2022_all_ages
+=#
+function mortality()
+    mortality_dict = Dict()
+    file = open("excess_mortality.csv", "r")
+    readline(file)
+
+    # goes through each of the CSV lines
+    for line in readlines(file)
+        println(line)
+    end
+    close(file) 
+
+    return mortality_dict
+end
+
 main()
