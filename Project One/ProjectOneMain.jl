1ba5675fe283116bdf788f7d148d1320a275c7de
Brandon Kaplan <59454456+BrandonKaplan@users.noreply.github.com>
2022-02-19 02:48:31 -0800

    Add files via upload

--- Project One/ProjectOneMain.jl
+++ Project One/ProjectOneMain.jl
@@ -8,17 +8,28 @@ function main()
         print("Type '1' for Vaccination Records")
 
         user_response = readline()
-        if (user_response == "1")
-            VaccineDict = vaccinations()
+        println(user_response)
+        if (cmp(user_response, "1") == 0)
+            vaccinations()
         end
         println(user_response)
     end
 
 end
 
+#=
+Processes the file vaccinations.csv file with the format:
+location, iso_code, date, total_vaccinations, people_vaccinated, people_fully_vaccinated, total_boosters, 
+daily_vaccinations_raw, daily_vaccinations, total_vaccinations_per_hundred, people_vaccinated_per_hundred, 
+people_fully_vaccinated_per_hundred, total_boosters_per_hundred, daily_vaccinations_per_million, 
+daily_people_vaccinated, daily_people_vaccinated_per_hundred
+=#
 function vaccinations()
-    file = open("")
-    
+    file = open("vaccinations.csv", "r")
+    for line in readlines(file)
+        println(line)
+    end
+    close(file) 
 end 
 
 main()
