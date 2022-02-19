b5c3144d0ebb6e5b22b9f3c09df97c95e6e32f41
Brandon Kaplan <59454456+BrandonKaplan@users.noreply.github.com>
2022-02-19 03:15:13 -0800

    Going to work on this after a nap

--- Project One/ProjectOneMain.jl
+++ Project One/ProjectOneMain.jl
@@ -4,15 +4,23 @@ function main()
 
     while user_response != "quit"
         # list of commands:
-        print("Please Type a Command: ")
-        print("Type '1' for Vaccination Records")
+        println("Type '1' for Vaccination Records")
+        println("Please Type a Command Number: ")
 
         user_response = readline()
-        println(user_response)
+
+        # Vaccine commands 
         if (cmp(user_response, "1") == 0)
-            vaccinations()
+            vaccination_dict = vaccinations()
+            println("Type '1' and a Country Name to Retrieve Quantity of Vaccines")
+            println("Type '2' for the Top 10 Countries in terms of Vaccines")
+            println("Type '3' for the Bottom 10 Countries in terms of Vaccines")
         end
-        println(user_response)
+        # Mortality commands
+        if (cmp(user_response, "2") == 0)
+            println("Type '1' Mortality Across All 3 Years (2020, 2021, 2022)")
+            println("Type '2' Mortality Across All 3 Years")
+        end
     end
 
 end
@@ -25,11 +33,17 @@ people_fully_vaccinated_per_hundred, total_boosters_per_hundred, daily_vaccinati
 daily_people_vaccinated, daily_people_vaccinated_per_hundred
 =#
 function vaccinations()
+    # create a dictionary mapping locations to 
+    vaccination_dict = Dict()
     file = open("vaccinations.csv", "r")
+    readline(file)
+    # goes through each of the CSV lines
     for line in readlines(file)
         println(line)
     end
     close(file) 
+
+    return vaccination_dict
 end 
 
 main()
